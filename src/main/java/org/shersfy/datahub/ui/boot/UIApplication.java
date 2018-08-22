85910fb246b83e18dc078659898f03b2ca218a64
pengyang <pengyang@cmhi.chinamobile.com>
2018-08-23 01:07:54 +0800

    session shared

--- src/main/java/org/shersfy/datahub/ui/boot/UIApplication.java
+++ src/main/java/org/shersfy/datahub/ui/boot/UIApplication.java
@@ -3,9 +3,13 @@ package org.shersfy.datahub.ui.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @EnableDiscoveryClient
+@EnableZuulProxy
+@EnableSpringHttpSession
 @ComponentScan("org.shersfy.datahub.ui")
 @SpringBootApplication
 public class UIApplication {
--- src/main/java/org/shersfy/datahub/ui/controller/PageController.java
+++ src/main/java/org/shersfy/datahub/ui/controller/PageController.java
@@ -5,24 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class PageController {
-    
-    @RequestMapping("/")
-    public ModelAndView index() {
-        ModelAndView mv = new ModelAndView("redirect:/index.html");
-        return mv;
-    }
-    
-    @RequestMapping("/index")
-    public ModelAndView index1() {
-        ModelAndView mv = new ModelAndView("redirect:/index.html");
-        return mv;
-    }
+public class PageController extends BaseController{
     
     @RequestMapping("/index.html")
     public ModelAndView index2() {
         ModelAndView mv = new ModelAndView("index");
+        mv.addObject("sessionId", getRequest().getSession().getId());
         return mv;
+    }
+    
+    @RequestMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("redirect:/index.html");
+    }
+    
+    @RequestMapping("/index")
+    public ModelAndView index1() {
+        return new ModelAndView("redirect:/index.html");
     }
     
     @RequestMapping("/login")
